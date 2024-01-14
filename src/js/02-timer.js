16fa653155fe438532ce60ee192c1bc729ef72d3
Lena0912 <elenabelichenko9@gmail.com>
2024-01-14 21:37:37 +0200

    03

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,14 +1,6 @@
 // import flatpickr from 'flatpickr';
 // import 'flatpickr/dist/flatpickr.min.css';
 
-// const promise = new Promise((resolve) => {
-//     resolve(10);
-// });
 
-// promise.then(value => {
-//     new Promise(resolve => {
-//         resolve(value * 2);
-//     });
-// }).then(value => console.log(value)); 
 
 
